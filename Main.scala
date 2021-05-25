a834daa6848215b0d0352e0ba79309cb7915d991
Arnout Engelen <arnout@bzzt.net>
2021-05-25 19:00:08 +0200

    add a server side

--- Main.scala
+++ Main.scala
@@ -3,27 +3,37 @@ import akka.actor._
 import akka.http.scaladsl._
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.unmarshalling._
+import akka.http.scaladsl.server.Directives._
 
 object Main extends App {
   implicit val system: ActorSystem = ActorSystem()
   implicit val ec = system.dispatcher
 
-  val baseUrl = "https://akka.io";
-
   for {
-            response <- Http().singleRequest(
-                HttpRequest(
-                    method = HttpMethods.POST,
-                    uri = baseUrl + s"/submit",
-                    entity = HttpEntity( ContentTypes.`application/json`, """["value-1","value-2","value-3]""" )
-                )
-            )
-            body <- {
-                Unmarshal( response.entity ).to[ String ]
-            }
-        } yield {
-          println(body)
-            //val res = read[ PollResponse ]( body )
-            //UUID.fromString( res.jobId )
-        }
+    binding <- Http()
+      .newServerAt("localhost", 0)
+      .bind(entity(as[String]) { e =>
+        println(s"Got request $e")
+        complete("Response")
+      })
+    response <- {
+      val baseUrl = s"http://${binding.localAddress.getHostName}:${binding.localAddress.getPort}"
+      println(baseUrl)
+      Http().singleRequest(
+        HttpRequest(
+          method = HttpMethods.POST,
+          uri = baseUrl,
+          entity = HttpEntity(ContentTypes.`application/json`, """["value-1","value-2","value-3]""" )
+        )
+      )
+    }
+    body <- {
+      println("Got response");
+      Unmarshal( response.entity ).to[ String ]
+    }
+  } yield {
+    println(body)
+    //val res = read[ PollResponse ]( body )
+    //UUID.fromString( res.jobId )
+  }
 }
